7a2dcfeca026ebfc0784aa0eb420cfdcbf39de9a
kingmuffy <65783231+kingmuffy@users.noreply.github.com>
2021-08-13 12:59:35 +0400

    nodemon concurrently

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Header />
-      <main class="py-3">
+      <main className="py-3">
         <Container>
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
